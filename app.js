5de9e3b4e37931c869ead59af6f8ecb035065096
Duck3to <juanjosecotuc33@gmail.com>
2025-08-11 19:21:04 -0600

    Creación de la función Agregar Amigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    
+    // Corregir: usar trim() para eliminar espacios y validar correctamente
+    if (nombreAmigo.trim() === ""){
+        alert("Por favor, inserte un nombre");
+    }
+    else{
+        amigos.push(nombreAmigo)
+        document.getElementById('amigo').value = "";
+    }
+    console.log(amigos);
+}
+
+
